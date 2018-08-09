7322ea0b285beb613a278e5182bffacd30cdd5f1
Praneeth Kumar Manala <pmanala@CGI.com>
2018-08-09 18:12:06 +0530

    Commiting Automation Project

---
+++ src/test/java/Demo/automation/Demo.java
@@ -0,0 +1,27 @@
+package Demo.automation;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	
+       @Test
+		public void test1() {
+    	   
+		System.out.println("Hello World");
+		
+		}
+       @Test
+		public void test2() {
+   	   
+		System.out.println("Hello World");
+		
+		}
+       @Test
+		public void test3() {
+   	   
+		System.out.println("Hello World");
+		
+		}
+	
+}
